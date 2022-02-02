e19bca4a30833ad1f2be2533f7ea48b2a9bd4a62
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2022-02-01 17:30:08 -0800

    solved multiple pointer challenge

--- ProblemSolvingPatterns/countUniqueValuesChallenge.js
+++ ProblemSolvingPatterns/countUniqueValuesChallenge.js
@@ -1,11 +1,36 @@
 // Count Unique Values Challenge with Multiple Pointers
 
+const countUniqueValues = (sortedArr) => {
+  let counter = 0;
+  let p1 = 0;
+  let p2 = 1;
+
+  while (p1 < sortedArr.length) {
+    if (sortedArr[p1] !== sortedArr[p2]) {
+      counter += 1
+      p1 = p2
+    } else {
+      p2++
+    }
+
+  }
+
+  return counter;
+
+}
+
+console.log(countUniqueValues([1,1,1,1,1,2]));
+
 /*
 Rules/Assumptions
-- array will be sorted with only numbers in it
+- array will be sorted with only numbers in it and can contain negatives
 
 Step 1: 
-- 
+- return a count/sum of the numbers that only show up once (unique) in the passed in array. USE THE MULTIPLE POINTERS PATTERN.
+- inputs - sorted array of numbers
+- ouput - sum/integer value representing the unique numbers in the sorted array
+- data = sorted array, a counter/sum to keep track of unique values, pointers
+- it is a sorted array = meaning that duplicates of a number will be next to each other and this will continue until the next largest number comes up. 
 
 
 Step 2: 
@@ -15,5 +40,10 @@ ex - [] : 0 unique values
 ex - [-2,-1,-1,0,1] : 4 unique values
 
 Step 3:
+- set a counter value to equal 0; set pointer1 to 0 ; set pointer 2 to 1
+- create a while loop? while p1 < arr.length
+  - compaer p1 to p2:
+    if p1 !== p2 ; counter += 1 and p1 = p2 p2 += 1
+    else p2++
 
 */
