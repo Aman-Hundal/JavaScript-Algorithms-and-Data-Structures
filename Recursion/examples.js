e3aa888b44b469142d40722d2a16fe59dfe4b193
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2022-02-10 13:34:27 -0800

    lecture on recursion started

--- Recursion/examples.js
+++ Recursion/examples.js
@@ -1,1 +1,18 @@
-// Example 1: Martin and the Dragon
+// Example 1: Martin and the Dragon. Return true if odd is found, if odd is not found return false.
+
+const dragonAnswer = (martinList) => {
+
+  if (martinList.length === 0) {
+    console.log("NO ODD")
+    return false;
+  }
+  if (martinList[0] % 2 !== 0) {
+    console.log("ODD FOUND")
+    return true;
+  }
+
+  dragonAnswer(martinList.splice(1));
+  
+}
+
+dragonAnswer([3142, 3, 6550, 5914]);
